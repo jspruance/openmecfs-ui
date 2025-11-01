f4eda595c79ebf6cf5ee59f3639dbfb19d20bfcf
Jonathan Spruance <jgspruance@gmail.com>
2025-10-31 21:48:46 -0400

    Adding AI Cure

--- src/types/react-plotly.d.ts
+++ src/types/react-plotly.d.ts
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * Minimal type declaration for react-plotly.js
+ * to silence TypeScript until an official type package exists.
+ */
+
 declare module "react-plotly.js" {
   import { Component } from "react";
-  interface PlotParams {
+
+  export interface PlotParams {
     data: any[];
     layout?: Record<string, any>;
     config?: Record<string, any>;
     style?: Record<string, any>;
     onClick?: (event: any) => void;
   }
+
   export default class Plot extends Component<PlotParams> {}
 }
