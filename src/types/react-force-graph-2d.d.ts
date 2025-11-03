5fd9d91ed1537229be46e6a2ca0076c5d12cdaf0
Jonathan Spruance <jgspruance@gmail.com>
2025-11-03 17:24:24 -0500

    Add mini graph

--- src/types/react-force-graph-2d.d.ts
+++ src/types/react-force-graph-2d.d.ts
@@ -1,1 +1,21 @@
-declare module "react-force-graph-2d";
+declare module "react-force-graph-2d" {
+  import { ComponentType } from "react";
+
+  export interface ForceGraphProps {
+    graphData: any;
+    width?: number;
+    height?: number;
+    backgroundColor?: string;
+    nodeRelSize?: number;
+    cooldownTicks?: number;
+    nodeCanvasObject?: (
+      node: any,
+      ctx: CanvasRenderingContext2D,
+      scale: number
+    ) => void;
+    onNodeClick?: (node: any) => void;
+  }
+
+  const ForceGraph2D: ComponentType<ForceGraphProps>;
+  export default ForceGraph2D;
+}
