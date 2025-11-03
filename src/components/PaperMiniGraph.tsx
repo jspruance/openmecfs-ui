54fd9ad4288cfffb1a97d437ed69ca9fb42ca0c3
Jonathan Spruance <jgspruance@gmail.com>
2025-11-03 18:00:10 -0500

    Add mini graph

--- src/components/PaperMiniGraph.tsx
+++ src/components/PaperMiniGraph.tsx
@@ -89,8 +89,8 @@ export default function PaperMiniGraph({ pmid }: { pmid: string }) {
           graphData={data}
           nodeRelSize={4}
           cooldownTicks={40}
-          linkColor={() => "rgba(148,163,184,0.4)"} // slate-400/40
-          linkWidth={() => 1}
+          //linkColor={() => "rgba(148,163,184,0.4)"} // slate-400/40
+          //linkWidth={() => 1}
           nodeCanvasObject={(node, ctx) => drawNode(node as GraphNode, ctx)}
         />
       </div>
