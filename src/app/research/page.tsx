0726be5e46f5960872b47cd6e5b41f823d9b2771
Jonathan Spruance <jgspruance@gmail.com>
2025-11-05 23:33:20 -0500

    fix Research LP graph - Cursor attempt

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -22,7 +22,7 @@ export default function ResearchIndexPage() {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Global Graph */}
-          <div className="lg:col-span-2">
+          <div className="lg:col-span-3">
             <LiveGraphCard />
           </div>
 
